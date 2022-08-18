ec837671462fb20d309555ae2de46c1093b465c7
jhahn678 <jhahn678@yahoo.com>
2022-08-18 12:28:04 -0400

    Errors for undefined and missing request params

---
+++ src/utils/errors/RequestError.ts
@@ -0,0 +1,35 @@
+type ErrorType = 
+| 'DUPLICATE_CONTACT' | 'DUPLICATE_CONTACT_REQUEST'
+| 'DELETE_NOT_FOUND' | 'RESOURCE_NOT_FOUND'
+| 'TRANSACTION_NOT_FOUND' | 'REQUEST_UNDEFINED'
+| 'REQUEST_FAILED'
+
+export class RequestError extends Error{
+    status: number = 400;
+    message: string;
+    constructor(errorType: ErrorType){
+        super()
+        switch(errorType){
+            case 'REQUEST_UNDEFINED':
+                this.message = 'The request parameters sent are missing or undefined'
+            case 'DUPLICATE_CONTACT':
+                this.message = 'User already on contact list'
+                break;
+            case 'DUPLICATE_CONTACT_REQUEST':
+                this.message = 'Contact request already exists to this user'
+                break;
+            case 'DELETE_NOT_FOUND':
+                this.message = 'Provided resource could not be deleted because it does not exist';
+                break;
+            case 'RESOURCE_NOT_FOUND':
+                this.message = 'The resource you requested does not exist';
+                break;
+            case 'TRANSACTION_NOT_FOUND':
+                this.message = 'The provided request could not be completed because a resource you provided does not exist';
+                break;
+            case 'REQUEST_FAILED':
+                this.message = 'The provided request could not be completed';
+                break;
+        }
+    }
+}
