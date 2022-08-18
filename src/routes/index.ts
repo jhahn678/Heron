b9b7cf21d52c1b5592b4a9ea7328c0b6ccdd204d
jhahn678 <jhahn678@yahoo.com>
2022-08-18 12:33:54 -0400

    Testing endpoint

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,14 @@ import { Router } from 'express'
 import { errorRequestHandler } from '../controllers/error';
 import AuthRouter from './auth'
 
+import knex from '../db/knex'
+
 const router = Router();
+
+router.get('/dev', async(req, res) => {
+    const users = await knex('users').where({ id: 1 }).update({bio: 'horse'}).returning('*')
+    res.status(200).json(users)
+})
 
 router.use('/auth', AuthRouter)
 router.get('/', (req, res) => res.send('This is the Heron API'))
