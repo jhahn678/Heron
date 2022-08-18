d3c11d7428f865ced53ccaa3197385c0347267a8
jhahn678 <jhahn678@yahoo.com>
2022-08-18 16:59:27 -0400

    Testing

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,13 +2,18 @@ import { Router } from 'express'
 import { errorRequestHandler } from '../controllers/error';
 import AuthRouter from './auth'
 
+
+import { polygon } from '@turf/helpers'
 import knex from '../db/knex'
 
 const router = Router();
 
 router.get('/dev', async(req, res) => {
-    const users = await knex('users').where({ id: 1 }).update({bio: 'horse'}).returning('*')
-    res.status(200).json(users)
+    // const users = await knex('users').where({ id: 1 }).update({bio: 'horse'}).returning('*')
+    const geom = polygon([[ [12, 21], [13, 24], [14, 25], [15, 26], [12, 21]]])
+    console.log(geom.geometry)
+    console.log(`${JSON.stringify(geom.geometry)}`)
+    res.status(200).json(geom.geometry)
 })
 
 router.use('/auth', AuthRouter)
