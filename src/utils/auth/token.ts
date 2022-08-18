c9caac009e2437694ceccf898e3d7afb2a04afeb
jhahn678 <jhahn678@yahoo.com>
2022-08-18 12:31:04 -0400

    basic auth token logic

--- src/utils/auth/token.ts
+++ src/utils/auth/token.ts
@@ -1,12 +1,26 @@
 import * as jwt from 'jsonwebtoken'
-import { AuthError } from '../errors/AuthError'
 
-interface TokenPayload {
+export interface TokenPayload {
     id: number
 }
 
+/** Returns signed JWT */
 export const createAuthToken = (payload: TokenPayload): string => {
     return jwt.sign(payload, process.env.JWT_SECRET!, {
         expiresIn: '7d'
     })
 }
+
+/** Returns userID from auth header if jsonwebtoken is valid -- else returns UNDEFINED */
+export const verifyAuthHeader = (authorizationHeader: string | undefined): number | undefined => {
+    try{
+        if(!authorizationHeader) return undefined;
+        const token = jwt.verify(authorizationHeader, process.env.JWT_SECRET!);
+        if(token && typeof token === 'object' && token.hasOwnProperty('id')){
+            return token.id;
+        }
+    }catch{
+        return undefined
+    }
+}
+
