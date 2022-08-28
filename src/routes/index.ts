d8d7fcf39fb28ce829341c466c16fada480eccab
jhahn678 <jhahn678@yahoo.com>
2022-08-28 11:37:35 -0400

    New datasource for waterbodies

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,26 +4,6 @@ import AuthRouter from './auth'
 import UploadRouter from './upload'
 const router = Router();
 
-
-
-
-
-import { polygon } from '@turf/helpers'
-import knex from '../configs/knex'
-router.get('/dev', async(req, res) => {
-    // const users = await knex('users').where({ id: 1 }).update({bio: 'horse'}).returning('*')
-    const geom = polygon([[ [12, 21], [13, 24], [14, 25], [15, 26], [12, 21]]])
-    console.log(geom.geometry)
-    console.log(`${JSON.stringify(geom.geometry)}`)
-    res.status(200).json(geom.geometry)
-})
-
-
-
-
-
-
-
 router.use('/auth', AuthRouter)
 router.use('/upload', UploadRouter)
 router.get('/', (req, res) => res.send('This is the Heron API'))
