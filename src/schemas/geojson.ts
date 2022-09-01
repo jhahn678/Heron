a638ae71ac36a0fbd43a58f450c8810b1749d62e
jhahn678 <jhahn678@yahoo.com>
2022-09-01 15:02:25 -0400

    Added proper scalars for geojson types

---
+++ src/schemas/geojson.ts
@@ -0,0 +1,31 @@
+import { gql } from "apollo-server-core";
+
+import {
+    MultiLineString,
+    MultiPolygon,
+    Geometry,
+    GeometryCollection,
+    Point,
+    Polygon,
+    LineString
+} from 'graphql-geojson-scalar-types'
+
+export const typeDef = gql`
+    scalar MultiLineString
+    scalar MultiPolygon
+    scalar GeometryCollection
+    scalar Geometry
+    scalar Point
+    scalar Polygon
+    scalar LineString
+`
+
+export const resolver = {
+    MultiLineString,
+    MultiPolygon,
+    GeometryCollection,
+    Geometry,
+    Point,
+    Polygon,
+    LineString
+}
