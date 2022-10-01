12834a7b9efb1c9d633ac405e9f69e7968fd65a6
jhahn678 <jhahn678@yahoo.com>
2022-09-30 21:35:22 -0400

    Nearest waterbodies route for new catch screen

--- src/routes/autocomplete.ts
+++ src/routes/autocomplete.ts
@@ -5,5 +5,6 @@ const router = Router()
 router.get('/', controllers.autocompleteAll)
 router.get('/geoplaces', controllers.autocompleteGeoplaces)
 router.get('/waterbodies', controllers.autocompleteWaterbodies)
+router.get('/nearest-waterbodies', controllers.nearestWaterbodies)
 
 export default router;
