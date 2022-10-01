ed520281a5007066f805c6007d7038154563b4b4
jhahn678 <jhahn678@yahoo.com>
2022-09-30 21:32:26 -0400

    Added new messages

--- src/utils/errors/AutocompleteQueryError.ts
+++ src/utils/errors/AutocompleteQueryError.ts
@@ -1,6 +1,7 @@
 type ErrorType = 
 | 'VALUE_REQUIRED'
 | 'COORDINATES_NOT_WITHIN_BOUNDARY'
+| 'LATLNG_NOT_PROVIDED'
 
 export class AutocompleteQueryError extends Error {
     message: string = 'There was an error with the provided query'
@@ -14,6 +15,10 @@ export class AutocompleteQueryError extends Error {
                 break;
             case 'COORDINATES_NOT_WITHIN_BOUNDARY':
                 this.message = 'The coordinates provided are not within the current dataset of North America'
+                break;
+            case 'LATLNG_NOT_PROVIDED':
+                this.message = 'LatLng is required for this query'
+                break;
         }
     }
 }
