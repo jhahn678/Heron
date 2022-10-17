cc07d4ffbc6409ce9702a8b23390790d39389012
jhahn678 <jhahn678@yahoo.com>
2022-10-17 11:58:11 -0400

    Added delete user resolver -- catch statistics bug fixes

--- src/schemas/user.ts
+++ src/schemas/user.ts
@@ -5,6 +5,9 @@ import { CatchStatisticsRes, LocationStatisticsRes, UserDetailsUpdate } from '..
 import { RequestError } from '../utils/errors/RequestError'
 import { UploadError } from '../utils/errors/UploadError'
 import { validateMediaUrl } from '../utils/validations/validateMediaUrl'
+import { DeleteObjectsCommand } from '@aws-sdk/client-s3'
+import S3Client from '../configs/s3'
+const { S3_BUCKET_NAME } = process.env;
 
 export const typeDef =  gql`
     type User { 
@@ -88,6 +91,7 @@ export const typeDef =  gql`
         updateUserAvatar(avatar: MediaInput): String
         followUser(id: Int!): Int
         unfollowUser(id: Int!): Int
+        deleteUser: User
     }
 
     input UserDetails {
@@ -184,6 +188,48 @@ export const resolver: Resolvers = {
                 .where({ user: auth, following: id })
                 .del()
             return id;
+        },
+        deleteUser: async (_, __, { auth }) => {
+            if(!auth) throw new AuthenticationError('Authentication Required')
+            const { rows } = await knex.raw<{ rows: { key: string }[] }>(`
+                with del1 as (
+                    delete from user_avatars 
+                    where "user" = ? returning "key"
+                ), del2 as (
+                    delete from catch_media 
+                    where "user" = ? returning "key"
+                ), del3 as (
+                    delete from catch_map_images 
+                    where "user" = ? returning "key"
+                ), del4 as (
+                    delete from location_media 
+                    where "user" = ? returning "key"
+                ), del5 as (
+                    delete from location_map_images 
+                    where "user" = ? returning "key"
+                ), del6 as (
+                    delete from waterbody_media 
+                    where "user" = ? returning "key"
+                )
+                select "key" from del1
+                union all
+                select "key" from del2
+                union all
+                select "key" from del3
+                union all
+                select "key" from del4
+                union all
+                select "key" from del5
+                union all
+                select "key" from del6
+            `, new Array(6).fill(auth))
+            const keys = rows.map(x => ({ Key: x.key}))
+            await S3Client.send(new DeleteObjectsCommand({
+                Bucket: S3_BUCKET_NAME!,
+                Delete: { Objects: keys }
+            }))
+            const [res] = await knex('users').where('id', auth).del('*')
+            return res;
         }
     },
     User: {
@@ -368,18 +414,20 @@ export const resolver: Resolvers = {
                     )::int as total_catches,
                     1 as "row"
                     from (
-                        select species, count(species)
+                        select species, count(*)
                         from catches
                         where "user" = ?
+                        and not species IS NULL
                         group by species
                     ) as spec
                 ) q1
                 left join (
                     select json_agg(wbs) as waterbody_counts, 1 as "row"
                     from (
-                        select waterbody, count(waterbody)
+                        select waterbody, count(*)
                         from catches
                         where "user" = ?
+                        and not waterbody IS NULL
                         group by waterbody
                     ) as wbs
                 ) q2
@@ -400,9 +448,10 @@ export const resolver: Resolvers = {
                 total.count as total_locations from (
                     select json_agg(wbs) as jsonarr
                     from (
-                        select waterbody::int, count(waterbody)
+                        select waterbody::int, count('*')
                         from locations
                         where "user" = ?
+                        and not waterbody IS NULL
                         group by waterbody
                     ) as wbs
                 ) as wbscounts,
