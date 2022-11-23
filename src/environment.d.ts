6a3bb8ffb5d5736e2ce488a9422163052433e70a
jhahn678 <jhahn678@yahoo.com>
2022-11-22 19:21:45 -0500

    Type declarations

--- src/environment.d.ts
+++ src/environment.d.ts
@@ -1,28 +1,28 @@
 declare global {
-    namespace NodeJS {
-      interface ProcessEnv {
-        PORT: string
-        JWT_SECRET: string
-        ACCESS_TOKEN_SECRET: string
-        REFRESH_TOKEN_SECRET: string
-        PG_HOST: string
-        PG_PORT: number
-        PG_USER: string
-        PG_PASSWORD: string
-        PG_DBNAME: string
-        S3_ACCESS_KEY: string
-        S3_SECRET_KEY: string
-        S3_BUCKET_NAME: string
-        S3_BASE_URL: string
-        SES_SMTP_USERNAME: string
-        SES_SMTP_PASSWORD: string
-        SES_ACCESS_KEY: string
-        SES_SECRET_KEY: string
-        AWS_REGION: string
-        GOOGLE_EXPO_CLIENT_ID: string
-        PASSWORD_RESET_URL: string
-      }
+  namespace NodeJS {
+    interface ProcessEnv {
+      PORT: string
+      JWT_SECRET: string
+      ACCESS_TOKEN_SECRET: string
+      REFRESH_TOKEN_SECRET: string
+      PG_HOST: string
+      PG_PORT: number
+      PG_USER: string
+      PG_PASSWORD: string
+      PG_DBNAME: string
+      S3_ACCESS_KEY: string
+      S3_SECRET_KEY: string
+      S3_BUCKET_NAME: string
+      S3_BASE_URL: string
+      SES_SMTP_USERNAME: string
+      SES_SMTP_PASSWORD: string
+      SES_ACCESS_KEY: string
+      SES_SECRET_KEY: string
+      AWS_REGION: string
+      GOOGLE_EXPO_CLIENT_ID: string
+      PASSWORD_RESET_URL: string
     }
   }
+}
   
 export {}
--- src/index.d.ts
+++ src/index.d.ts
@@ -1,12 +1,6 @@
-declare module "express-serve-static-core" {
-    interface Request {
-        user?: number
-    }
-}
-
 declare module 'jsonwebtoken' {
-    interface JwtPayload {
-        id: number,
-        jti?: string
-    }
+  interface JwtPayload {
+      id: number,
+      jti?: string
+  }
 }
