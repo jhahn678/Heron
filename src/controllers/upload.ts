d5da92338f593dedb1cde74a577ea08c9f7478dd
jhahn678 <jhahn678@yahoo.com>
2022-11-22 20:44:17 -0500

    Integrating middleware with controlller'

--- src/controllers/upload.ts
+++ src/controllers/upload.ts
@@ -1,14 +1,12 @@
 import { Request } from 'express'
 import { asyncWrapper } from "../utils/errors/asyncWrapper";
-import { AuthError } from '../utils/errors/AuthError';
 import { UploadError } from '../utils/errors/UploadError';
-import { verifyAccessToken } from '../utils/auth/token';
 import { validateUploadType } from '../utils/validations/validateUploadType';
 import { v4 as uuid } from 'uuid'
 import S3Client from '../configs/s3';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
 import { PutObjectCommand } from '@aws-sdk/client-s3'
-
+import { AuthError } from '../utils/errors/AuthError';
 const { S3_BUCKET_NAME } = process.env;
 
 //Expiration for signed URL
@@ -19,17 +17,14 @@ interface SignedUrlReq {
     mimetype: string
 }
 
-export const getSignedUploadUrl = asyncWrapper(async (req: Request<{},{},{},SignedUrlReq>, res, next) => {
+export const getSignedUploadUrl = asyncWrapper(async (req: Request<{},{},{},SignedUrlReq>, res) => {
+    
     const { mimetype } = req.query
 
     if(!mimetype) throw new UploadError('FILE_TYPE_REQUIRED')
     if(!validateUploadType(mimetype)) throw new UploadError('INVALID_FILE_TYPE')
-    
-    const { authorization } = req.headers;
-    if(!authorization) throw new AuthError('AUTHENTICATION_REQUIRED')
-    const payload = verifyAccessToken(authorization.split(' ')[1], { error: 'EXPRESS' }) 
 
-    const fileKey = `${payload.id}-${uuid()}.${mimetype.split('/')[1]}`;
+    const fileKey = `${req.user}-${uuid()}.${mimetype.split('/')[1]}`;
 
     const putCommand = new PutObjectCommand({
         Bucket: S3_BUCKET_NAME,
