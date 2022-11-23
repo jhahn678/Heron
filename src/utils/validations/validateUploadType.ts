feeef31be2ed98f530d3ec225908014852286ac1
jhahn678 <jhahn678@yahoo.com>
2022-11-22 19:20:21 -0500

    Updated allowed mimetypes for images

--- src/utils/validations/validateUploadType.ts
+++ src/utils/validations/validateUploadType.ts
@@ -1,4 +1,4 @@
-const acceptedMimeTypes: string[] = ['image/png', 'image/jpeg', 'image/gif', 'image/bmp']
+const acceptedMimeTypes: string[] = ['image/png', 'image/jpeg', 'image/gif', 'image/svg+xml', 'image/webp']
 
 export const validateUploadType = (mimetype: string): Boolean => {
     return acceptedMimeTypes.includes(mimetype)
