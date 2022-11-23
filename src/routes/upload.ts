b592e855640ca8574169ce7921c1fcb516546b14
jhahn678 <jhahn678@yahoo.com>
2022-11-22 20:43:51 -0500

    Middleware added

--- src/routes/upload.ts
+++ src/routes/upload.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express'
 import controllers from '../controllers';
+import { requireAccessToken } from '../utils/middleware/auth';
+
 const router = Router();
 
-router.get('/signed-url', controllers.getSignedUploadUrl)
+router.get('/signed-url', requireAccessToken, controllers.getSignedUploadUrl)
 
 export default router
