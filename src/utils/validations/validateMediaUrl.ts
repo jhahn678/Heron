04ded4589c03acb7fd50ca99d64ecfbe49a3f3cf
jhahn678 <jhahn678@yahoo.com>
2022-11-24 20:21:01 -0500

    refactor

--- src/utils/validations/validateMediaUrl.ts
+++ src/utils/validations/validateMediaUrl.ts
@@ -2,5 +2,5 @@ const { S3_BASE_URL } = process.env;
 
 export const validateMediaUrl = (url: string | undefined | null): Boolean => {
     if(typeof url !== 'string') return false;
-    return url.startsWith(S3_BASE_URL!)
+    return url.startsWith(S3_BASE_URL)
 }
